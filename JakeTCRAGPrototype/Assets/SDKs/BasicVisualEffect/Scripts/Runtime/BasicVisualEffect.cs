460b952432b9405cf098032e9ddc0fd2a4d480a3
Vincent <vviinncceenntt21@gmail.com>
2024-08-26 18:27:13 -0600

    Start working on BasicVisualEffect

---
+++ JakeTCRAGPrototype/Assets/SDKs/BasicVisualEffect/Scripts/Runtime/BasicVisualEffect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class BasicVisualEffect : MonoBehaviour
+{
+    [SerializeField] VisualEffect VE;
+    public int aa;
+    public bool bb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ JakeTCRAGPrototype/Assets/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : BasicVisualEffect
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
